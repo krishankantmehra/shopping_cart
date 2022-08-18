a54a203d9a480be547a61079097f119df40438db
Krishan Kant Mehra <krishankantmehra23@gmail.com>
2022-08-19 02:34:39 +0530

    V_3

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
                 <h4 className='text-center'><b>Contact Details</b></h4>
                 <div className='d-flex flex-row justify-content-center'>
                     <a href = "mailto: krishankantmehra@example.com" className='contact-links' id="googleLink" >
-                        <i className='fa fa-google'/>
+                        <i className='fa fa-envelope'/>
                     </a>
                     <a href = "https://github.com/krishankantmehra" className='contact-links' id="githubLink" target={"_blank"} rel="noreferrer">
                         <i className='fa fa-github'/>
